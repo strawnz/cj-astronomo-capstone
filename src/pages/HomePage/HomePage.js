d831936afd43035656ca495a00fe50466d41973f
C.J. Astronomo <cj.astronomo@gmail.com>
2024-01-31 19:07:00 -0500

    Add text content and link

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -9,7 +9,22 @@ function HomePage() {
                 <div className='hero__sports'></div>
                 <div className='hero__theatre'></div>
             </section>
-            <h1>Home Page Placeholder</h1>
+            <section className='home__container'>
+                <h1 className='home__header'>Pre-Event Planner</h1>
+                <p class='home__body'>
+                    Ready to transform your journey to a live event 
+                    from chaos to coordinated elegance?
+                </p>                
+                <p class='home__body'>
+                    Your path to unforgettable moments begins here.
+                    Click below to 'PEP' up your itinerary!
+                </p>
+            </section>
+            <section className='home__button-container'>
+                <Link to='/form' className='home__button'>
+                    START
+                </Link>
+            </section>
         </main>
     )
 };
