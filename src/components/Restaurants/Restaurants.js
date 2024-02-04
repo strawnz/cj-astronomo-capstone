f22b26fe2a193b7cdc33b2b73219c24df331cd3c
C.J. Astronomo <cj.astronomo@gmail.com>
2024-02-04 15:01:08 -0500

    Create useEffect to get restaurant info for each restaurant option

--- src/components/Restaurants/Restaurants.js
+++ src/components/Restaurants/Restaurants.js
@@ -6,14 +6,14 @@ function Restaurants({ venueId, venueName, onSelect, priceChoice }) {
     console.log("Venue Id from Itinerary Form to Restaurants: ", venueId); // remove this eventually
     console.log("Price Choice from Itinerary Form to Restaurants: ", priceChoice); // remove this eventually
     const [restoOptions, setRestoOptions] = useState([]);
-    const [restoOptionsByPrice, setRestoOptionsByPrice] = useState([]);
+    const [restoInfo, setRestoInfo] = useState([]);
     const [selectedRestoId, setSelectedRestoId] = useState(null);
 
     useEffect(() => {
         const fetchRestoOptions = async () => {
           try {
             const response = await axios.get(`
-                    http://localhost:8080/api/restos-venues/by-venue?venue_id=${venueId}&price_level=${priceChoice}`);
+                http://localhost:8080/api/restos-venues/by-venue?venue_id=${venueId}&price_level=${priceChoice}`);
             setRestoOptions(response.data);
             console.log("Restaurant options by venue ID and price level: ", response.data); // remove this eventually
           } catch (error) {
@@ -24,6 +24,27 @@ function Restaurants({ venueId, venueName, onSelect, priceChoice }) {
           fetchRestoOptions();
         }
       }, [venueId, priceChoice]);
+
+    useEffect(() => {
+        const fetchRestoInfo = async () => {
+            try {
+                const promises = restoOptions.map(async (option) => {
+                    const response = await axios.get(`
+                        http://localhost:8080/api/restaurants/by-venue/${option.resto_id}`);
+                    return response.data;
+                });
+
+                const restoInfoArray = await Promise.all(promises)
+                setRestoInfo(restoInfoArray);
+                console.log("Restaurant info from options: ", restoInfoArray); // remove this eventually
+            } catch (error) {
+                console.log("Error fetching restaurant info: ", error);
+            }
+        }
+        if (restoOptions.length > 0) {
+            fetchRestoInfo();
+        }
+    }, [restoOptions]);
     
     return (
         <h1>Restaurants Component Placeholder</h1>
